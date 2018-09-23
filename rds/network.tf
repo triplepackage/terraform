411a0c1d8d3f8d08d51eac4ac39477e86a575a08
John <admin@tests-MBP-3.fios-router.home>
2018-09-23 09:39:31 -0400

    Removed Flask service listeners to keep things tidy.

--- rds/network.tf
+++ rds/network.tf
@@ -243,14 +243,6 @@ resource "aws_alb_target_group" "rest_service" {
   target_type = "ip"
 }
 
-resource "aws_alb_target_group" "flask_service" {
-  name        = "tf-ecs-flask"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = "${aws_vpc.default.id}"
-  target_type = "ip"
-}
-
 # Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "rest_service" {
   load_balancer_arn = "${aws_alb.main.id}"
@@ -259,17 +251,6 @@ resource "aws_alb_listener" "rest_service" {
 
   default_action {
     target_group_arn = "${aws_alb_target_group.rest_service.id}"
-    type             = "forward"
-  }
-}
-
-resource "aws_alb_listener" "flask_service" {
-  load_balancer_arn = "${aws_alb.main.id}"
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = "${aws_alb_target_group.flask_service.id}"
     type             = "forward"
   }
 }
