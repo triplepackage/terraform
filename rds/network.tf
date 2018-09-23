65592695d8202d660040ebb2c2c6c06b514a3a3e
John <admin@tests-MBP-3.fios-router.home>
2018-09-23 09:10:28 -0400

    Added and verified ECS portiion.

--- rds/network.tf
+++ rds/network.tf
@@ -273,3 +273,42 @@ resource "aws_alb_listener" "flask_service" {
     type             = "forward"
   }
 }
+
+### ECS
+
+resource "aws_ecs_cluster" "main" {
+  name = "halifax"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                    = "rental-app"
+  network_mode              = "awsvpc"
+  requires_compatibilities  = ["FARGATE"]
+  execution_role_arn        = "arn:aws:iam::002609254882:role/ecsTaskExecutionRole"
+  cpu                       = "1024"
+  memory                    = "2048"
+  container_definitions = "${file("./task-def.json")}"
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "tf-ecs-service"
+  cluster         = "${aws_ecs_cluster.main.id}"
+  task_definition = "${aws_ecs_task_definition.app.arn}"
+  desired_count   = "${var.app_count}"
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups = ["${aws_security_group.ecs_tasks.id}"]
+    subnets         = ["${aws_subnet.private_subnet.*.id}"]
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.rest_service.id}"
+    container_name   = "rental-services-api-jpa-stage"
+    container_port   = "${var.app_port}"
+  }
+
+  depends_on = [
+    "aws_alb_listener.rest_service"
+  ]
+}
